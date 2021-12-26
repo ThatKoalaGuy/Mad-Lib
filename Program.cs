330a90a33fa60b60751b1bfe8de6fa8a2c9a1305
Domen Koder <domen.koder@gmail.com>
2021-12-26 13:22:51 +0100

    Full Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string color, PluralNoun, celebrity;
+
+
+Console.WriteLine("Write a color");
+color=Console.ReadLine();
+
+Console.WriteLine("Write a plural noun");
+PluralNoun = Console.ReadLine();
+
+Console.WriteLine("Write a celebrity");
+celebrity = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"Roses are {color}");
+Console.WriteLine($"{PluralNoun} are blue");
+Console.WriteLine($"I love {celebrity}");
